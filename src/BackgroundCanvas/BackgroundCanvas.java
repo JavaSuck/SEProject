2d28954828aab47fb0444fdd06fc4a32994f48aa
c83228 <c83228@gmail.com>
2016-12-22 00:12:45 +0800

    BackgroungCanvas update

--- src/BackgroundCanvas/BackgroundCanvas.java
+++ src/BackgroundCanvas/BackgroundCanvas.java
@@ -5,17 +5,17 @@ import javax.swing.*;
 
 public class BackgroundCanvas {
 
-    private int xOffset ;
-    private int yOffset ;
+    public int xOffset ;
+    public int yOffset ;
 
     public void renderScene(Image map){
 
     }
 
 
-    public int move(int xOffsetDelta,int yOffsetDelta){
+    public void move(int xOffsetDelta,int yOffsetDelta){
         xOffset += xOffsetDelta;
         yOffset += yOffsetDelta;
-        return xOffset,;
+
     }
 }

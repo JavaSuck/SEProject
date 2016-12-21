27afbb651a0ee76f4ed7379aa0fdc0b5601195dd
c83228 <c83228@gmail.com>
2016-12-21 22:58:03 +0800

    SDM init

---
+++ src/BackgroundCanvas/BackgroundCanvas.java
@@ -0,0 +1,21 @@
+package BackgroundCanvas;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class BackgroundCanvas {
+
+    private int xOffset ;
+    private int yOffset ;
+
+    public void renderScene(Image map){
+
+    }
+
+
+    public int move(int xOffsetDelta,int yOffsetDelta){
+        xOffset += xOffsetDelta;
+        yOffset += yOffsetDelta;
+        return xOffset,;
+    }
+}
--- src/SceneRE/SceneRE.java
+++
@@ -1,4 +0,0 @@
-package SceneRE;
-
-public class SceneRE {
-}

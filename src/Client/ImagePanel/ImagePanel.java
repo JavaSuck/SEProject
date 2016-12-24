83b0c84255dc1eb2463bd351711d88138d580c4e
dmoon <dmoon.t@gmail.com>
2016-12-24 12:56:14 +0800

    Refactor imagePanel with overloading

--- src/Client/ImagePanel/ImagePanel.java
+++ src/Client/ImagePanel/ImagePanel.java
@@ -2,7 +2,9 @@ package Client.ImagePanel;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import Client.Sprite.*;
@@ -13,14 +15,33 @@ public class ImagePanel extends JPanel {
     private Image resizedImage;
     private int resizedWidth = 150;
     private int resizedHeight = 150;
-    private int splitSize = 144;
+    private int cropSize = 50;
 
-    public ImagePanel(String fileName) {
-        setPreferredSize(new Dimension(resizedWidth, resizedHeight));
+    public ImagePanel(String fileName, int width, int height) {
+        initUI(width, height);
+        try {
+            srcImage = ImageIO.read(new File("src/Client/Assets/Images/" + fileName));
+        } catch (Exception ex) {
+            System.out.println("Can't find " + fileName + "in Assets/Images/");
+        }
+        resizeSrcImage();
+    }
+
+    public ImagePanel(String fileName, int width, int height, int cropSize) {
+        initUI(width, height);
         Sprite avatarSprite = new Sprite();
+        this.cropSize = cropSize;
+        avatarSprite.setSplitSize(cropSize);
         avatarSprite.loadSprite(fileName);
-        avatarSprite.setSplitSize(splitSize);
         srcImage = avatarSprite.getSprite(0, 0);
+        resizeSrcImage();
+    }
+
+    private void initUI(int width, int height) {
+        setResizedSize(width, height);
+    }
+
+    private void resizeSrcImage() {
         resizedImage = srcImage.getScaledInstance(resizedWidth, resizedHeight, java.awt.Image.SCALE_SMOOTH);
     }
 
@@ -30,12 +51,13 @@ public class ImagePanel extends JPanel {
         g.drawImage(resizedImage, 0, 0, this); // see javadoc for more info on the parameters
     }
 
-    public void setSplitSize(int size) {
-        splitSize = size;
+    public void setCropSize(int size) {
+        cropSize = size;
     }
 
     public void setResizedSize(int width, int height) {
         resizedWidth = width;
         resizedHeight = height;
+        repaint();
     }
 }

55e9be0d56d3a48003feba3c2814177a9caeec98
Jasonnor <wujason810@gmail.com>
2016-12-25 22:16:44 +0800

    Fix server walk direction

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,6 +1,7 @@
 package Server;
 
 import Server.CDC.CDC;
+import Server.CDC.Direction;
 import Server.CDC.Player;
 import Server.TCPServer.TCPServer;
 import Server.TCPServer.Tool.fakeCDC;
@@ -14,7 +15,7 @@ public class Server {
         CDC cdc = new CDC();
         Player p = (Player) cdc.playerController.players.get(1);
         System.out.println(p.coordinate);
-        cdc.playerController.walk(1, 2);
+        cdc.playerController.walk(1, Direction.DOWN);
         System.out.println(p.coordinate);
 
         // TODO: Change it to CDC

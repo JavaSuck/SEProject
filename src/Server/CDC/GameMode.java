647771a6c945bbfe7531745009a706c0f1aee909
Jasonnor <wujason810@gmail.com>
2016-12-26 01:00:31 +0800

    Add server ip todo

--- src/Server/CDC/GameMode.java
+++ src/Server/CDC/GameMode.java
@@ -3,6 +3,7 @@ package Server.CDC;
 import java.net.InetAddress;
 
 public class GameMode {
+    // TODO: set it as static ip of one computer
     public static InetAddress serverAddress;
     public static final int playerCount = 4;
     public static final int personalMaxBomb = 4;

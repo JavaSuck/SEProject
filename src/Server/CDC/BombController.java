253f2e5264498b8446ea32a1cf65b8955922874e
Jasonnor <wujason810@gmail.com>
2016-12-25 18:40:41 +0800

    Add Bomb

--- src/Server/CDC/BombController.java
+++ src/Server/CDC/BombController.java
@@ -1,7 +1,27 @@
 package Server.CDC;
 
-public class BombController {
-  public BombController(GameMap gameMap) {
+import java.awt.*;
+import java.util.ArrayList;
 
-  }
+class BombController {
+    public GameMap gameMap;
+    public ArrayList<Bomb> bombs;
+
+    BombController(GameMap gameMap) {
+        this.gameMap = gameMap;
+        this.bombs = new ArrayList<>();
+    }
+
+    private void timer() {
+
+    }
+
+    private Boolean generate(int playerId, Point coordinate) {
+        bombs.add(new Bomb(playerId, coordinate, 100, 3));
+        return true;
+    }
+
+    private void explode(Bomb bomb) {
+
+    }
 }

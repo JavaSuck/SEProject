cd9946064017757827a61bf725ef6911cf26b04f
Jasonnor <wujason810@gmail.com>
2016-12-26 11:28:44 +0800

    Set server ip as static ip

--- src/Server/CDC/GameMode.java
+++ src/Server/CDC/GameMode.java
@@ -1,6 +1,7 @@
 package Server.CDC;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class GameMode {
     // TODO: set it as static ip of one computer
@@ -9,4 +10,12 @@ public class GameMode {
     public static final int personalMaxBomb = 4;
     public static final int bombExpireTime = 5;
     public static final double walkSpeed = 0.5;
+
+    public GameMode() {
+        try {
+            serverAddress = InetAddress.getByName("140.115.51.94");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,32 +2,25 @@ package Server;
 
 import Server.CDC.CDC;
 import Server.CDC.Direction;
-import Server.CDC.GameMode;
 import Server.CDC.Player;
 import Server.TCPServer.TCPServer;
 import Server.UDPServer.UDPServer;
 
 import java.io.IOException;
-import java.net.InetAddress;
 
 public class Server {
 
     public Server() throws IOException {
-        GameMode.serverAddress = InetAddress.getLocalHost();
-
         CDC cdc = new CDC();
         Player p = cdc.playerController.players.get(1);
         System.out.println(p.coordinate);
         cdc.playerController.walk(1, Direction.DOWN);
         System.out.println(p.coordinate);
 
-        // TODO: Get connection_limit from gameMode
         TCPServer tcp = new TCPServer(40689, cdc);
-
         tcp.initTCPServer();
 
         UDPServer udp = new UDPServer(tcp, cdc);
         udp.start();
-
     }
 }

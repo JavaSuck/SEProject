10a8070748cfa5c3fb831bc2c7722880822bd7fb
dmoon <dmoon.t@gmail.com>
2016-12-28 06:48:40 +0800

    Add explosion

---
+++ src/Client/Bomb/Explosion.java
@@ -0,0 +1,24 @@
+package Client.Bomb;
+
+import java.util.HashMap;
+
+import javax.swing.*;
+
+import Client.Sprite.Sprite;
+
+
+public class Explosion extends Sprite {
+
+    private int power = 2;
+//    private HashMap<int, Sprite> explosion = new HashMap<>;
+
+    public Explosion() {
+        loadSprite("bomb.png");
+    }
+
+    public void initExplosion() {
+        for (int i = 0; i < Math.pow(2 * power + 1, 2); i++) {
+
+        }
+    }
+}

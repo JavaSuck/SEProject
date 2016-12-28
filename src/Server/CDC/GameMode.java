acb80016e795921729ee71f1adc618e7da0c1f47
Jasonnor <wujason810@gmail.com>
2016-12-29 00:19:05 +0800

    Add UdpPlayerCount

--- src/Server/CDC/GameMode.java
+++ src/Server/CDC/GameMode.java
@@ -1,10 +1,11 @@
 package Server.CDC;
 
 public class GameMode {
-    public static final String serverAddress = "127.0.0.1";
+    public static final String serverAddress = "192.168.1.213";
     public static final int TCPPort = 5567;
     public static final int UDPPort = 5566;
     public static final int playerCount = 4;
+    public static final int UdpPlayerCount = 2;
     public static final int playerMaxBomb = 3;
     public static final int bombExpireTime = 3000;
     public static final int bombPower = 5;

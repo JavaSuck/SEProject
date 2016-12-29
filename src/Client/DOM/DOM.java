95c83371e8899e92f5d3fd257912407df0d873c3
tz5514 <tz5514@gmail.com>
2016-12-30 06:59:43 +0800

    optimize

--- src/Client/DOM/DOM.java
+++ src/Client/DOM/DOM.java
@@ -73,26 +73,16 @@ public class DOM {
     }
 
     public void updateVirtualCharacter(int playerId, Direction dir, Point coordinateNext, boolean shouldCharacterSync, int deadTime) {
-        if (deadTime != 0) {
-            characters.get(playerId).dead();
-            sidebar.updateAvatarBox(playerId);
-        } else {
-            characters.get(playerId).updateCharacter(dir, coordinateNext, shouldCharacterSync);
-        }
+        new Thread(() -> {
+            if (deadTime != 0) {
+                characters.get(playerId).dead();
+                sidebar.updateAvatarBox(playerId);
+            } else {
+                characters.get(playerId).updateCharacter(dir, coordinateNext, shouldCharacterSync);
+            }
+        }).start();
     }
 
-    public void addItem(String name, int index, boolean shared) {
-        Item newItem = new Item(name, index, shared, "item.png");
-        if (dynamicObjects.get(index) == null) {
-            dynamicObjects.put(index, newItem);
-        } else {
-            System.out.println("Create Failed: item already exist");
-        }
-    }
-
-    public void updateItem(int index, boolean shared, int owner) {
-        dynamicObjects.get(index).updateItem(shared, owner);
-    }
 
     public void updateBomb(int index, int x, int y, boolean isExist, int[] explosionRange, int power) {
         new Thread(() -> {
@@ -116,17 +106,7 @@ public class DOM {
                 backgroundCanvas.add(newExplosion);
                 newExplosion.startAnimation();
             }
-
         }).start();
-    }
-
-    public void bombExplode(int index) {
-        if (bombs.get(index) == null) {
-//            bombs.get(index);
-//            create Explosion(int x, int y);
-//            backgroundCanvas.add(explosion);
-//            explosion.start();
-        }
     }
 
     public HashMap<Integer, Item> getAllDynamicObjects() {

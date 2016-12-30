cacff86dcf8ec1ce4bfaea7799e2709554be9072
Jasonnor <wujason810@gmail.com>
2016-12-30 11:03:24 +0800

    Update avatar refresh

--- src/Client/UIComponents/Sidebar.java
+++ src/Client/UIComponents/Sidebar.java
@@ -10,22 +10,31 @@ public class Sidebar extends JPanel {
     private final int width = 165;
     private final int height = 720;
     private int updateCount = 0;
-    private AvatarBox[] avatarBoxs = new AvatarBox[4];
+    private AvatarBox[] avatarBoxes;
+    private boolean[] isDead;
 
     public Sidebar() {
+        avatarBoxes = new AvatarBox[4];
+        isDead = new boolean[4];
+        for (int i = 0; i < 4; i++) {
+            isDead[i] = false;
+        }
         setBackground(new Color(35, 105, 186));
     }
 
     public void addAvatarBox(int id, String name) {
         if (id == updateCount) {
-            avatarBoxs[id] = new AvatarBox(name, "avatar" + id + ".png");
-            add(avatarBoxs[id]);
+            avatarBoxes[id] = new AvatarBox(name, "avatar" + id + ".png");
+            add(avatarBoxes[id]);
             ++updateCount;
         }
     }
 
     public void updateAvatarBox(int id) {
-        avatarBoxs[id].updateAvatar("avatar" + id + "_dead.png");
+        if (!isDead[id]) {
+            avatarBoxes[id].updateAvatar("avatar" + id + "_dead.png");
+            isDead[id] = true;
+        }
     }
 
     @Override
